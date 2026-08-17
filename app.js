3a10cb0da3ce2659486e46b9a9290811fb03e2ad
NandishBadami <nandishbadamigoogly@gmail.com>
2026-08-17 16:30:28 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,21 @@
+const { GoogleGenAI } = require('@google/genai');
+const express = require('express');
+
+const app = express();
+const ai = new GoogleGenAI({});
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app.get('/', async(req, res) => {
+    if(req.query.prompt) {
+        const result = await ai.interactions.create({
+            model: 'gemini-3.6-flash',
+            input: req.query.prompt
+        });
+        return res.render('index.ejs', {prompt: req.query.prompt, result: result.output_text})
+    } 
+    res.render('index.ejs');
+});
+
+app.listen(3000, () => console.log('http://localhost:3000'))
---
+++ readme.md
@@ -0,0 +1,4 @@
+# Express-Gemini-Web-App
+
+A web app where users can chat with gemini-3.6-flash google's ai model from my website. Built using node/express and gemini api. Live Website link:
+
